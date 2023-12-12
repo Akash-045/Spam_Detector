8609d831808f050a9c5a169d06e6aeb0bcaf0b72
Akash <samantray745@gmail.com>
2023-12-12 12:32:09 +0100

    Explanation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,9 +9,14 @@ public class Main {
     private static final List<String> emailBodies = new ArrayList<>();// List to store email bodies
     private static final double SPAM_THRESHOLD = 0.5; // Threshold to determine if an email is spam
 
-
+    /**
+     * The main method initializes the spam detection process. It simulates a simple
+     * scenario by adding predefined email bodies and then calculates their spam probability.
+     * This method demonstrates how the spam detector could be used in a real application,
+     * serving as a basic example of its functionality.
+     */
     public static void main(String[] args) {
-        // Main method - program execution starts here
+
         System.out.println("Spam Detector is running");
 
         addEmailBody("I want to play everyday");
@@ -20,21 +25,45 @@ public class Main {
 
         calculateSpamProbability();
     }
-
+    /**
+     * This method adds an email body to the list for analysis. It's essential for building
+     * the dataset that the spam detection algorithm will analyze. By adding emails to this list,
+     * we can simulate a real-world scenario where emails are continuously received and need
+     * to be analyzed for spam.
+     *
+     * @param emailBody The content of the email to be added.
+     */
     private static void addEmailBody(String emailBody) {
-        emailBodies.add(emailBody);// Adds an email body to the list
+
+        emailBodies.add(emailBody);
     }
-    // Calculates similarity between two email bodies
+    /**
+     * Calculates the similarity between two email bodies as a part of the spam detection logic.
+     * This method is based on the idea that spam emails often have similar content. By calculating
+     * the similarity between emails, we can identify potential spam. The similarity score helps in
+     * determining if emails are likely to be spam when they share a significant amount of common content.
+     *
+     * @param emailBody1 The first email body.
+     * @param emailBody2 The second email body.
+     * @return A double representing the similarity score (0 to 1).
+     */
     private static double calculateSimilarity(String emailBody1, String emailBody2) {
-        Set<String> words1 = new HashSet<> (Arrays.asList(emailBody1.split(" ")));// Create a set of words from the first email
-        Set<String> words2 = new HashSet<>(Arrays.asList(emailBody2.split(" ")));// Create a set of words from the second email
+
+        Set<String> words1 = new HashSet<> (Arrays.asList(emailBody1.split(" ")));
+        Set<String> words2 = new HashSet<>(Arrays.asList(emailBody2.split(" ")));
 
         Set<String> commonWords = new HashSet<>(words1);
-        commonWords.retainAll(words2);// Find common words between both sets
+        commonWords.retainAll(words2);
 
-        return (double) commonWords.size() / (words1.size() + words2.size() - commonWords.size());// Calculate and return the similarity score
+        return (double) commonWords.size() / (words1.size() + words2.size() - commonWords.size());
     }
-    // Calculates spam probability for each email
+    /**
+     * Analyzes each email's content and calculates the probability of it being spam.
+     * This method leverages the similarity scores between emails to make this determination,
+     * based on the assumption that high similarity among email bodies is a potential indicator of spam.
+     * This method is crucial in the spam detection process as it ultimately decides which emails are
+     * flagged as spam, demonstrating a basic approach to spam filtering.
+     */
     private static void calculateSpamProbability() {
         for (int i = 0; i < emailBodies.size(); i++) {
             double totalSimilarity = 0.0;// Total similarity score
